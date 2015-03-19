fa893e4d03547d8044b984db60fcd5a0bcb024e7
jgriffinNC <eighthgriffin@gmail.com>
2015-03-19 13:00:46 -0400

    Update TourPoint.java
    added comments.

--- WCUTour/src/models/TourPoint.java
+++ WCUTour/src/models/TourPoint.java
@@ -10,11 +10,16 @@ import com.google.android.gms.maps.model.LatLng;
 
 public class TourPoint {
 
-
+    /** ID of the pair of coordinates. */
     int id;
+    /** latitude value. */
     double lat;
+    /** longitude value. */
     double lng;
 
+    /**
+     * Constructor for TourPoint object. 
+     */
     public TourPoint(){
 
         this.id=0;
@@ -22,7 +27,12 @@ public class TourPoint {
         this.lng=0.0;
 
     }
-
+    /**
+     * Constructor for TourPoint object. 
+     * @param id - id of the point. 
+     * @param lat - latitude
+     * @param lng - longitude
+     */
     public TourPoint(int id, double lat, double lng){
 
         this.id = id;
@@ -30,31 +40,48 @@ public class TourPoint {
         this.lng = lng;
 
     }
-
-    public double getLatitude(){
-        return lat;
-    }
-
+    /**
+     * This method returns the latitude value of a point.
+     * @return double - latitude
+     */
+    public double getLatitude(){ return lat;}
+    /**
+     * This method returns the longitude value of a point.
+     * @return double - longitude
+     */
     public double getLongitude(){ return lng;}
-
+    /**
+     * This method returns the id of the point. 
+     * @return int - id
+     */
     public int getId(){ return this.id;}
-
-
-
+    /**
+     * This method sets the longitude value.
+     * @param value - longitude
+     */
     public void setLongitude(double value){  this.lng = value; }
-
-    public void setLatitude(double value){
-        this.lat = value;
-    }
-    public void setId(int value){
-        this.id = value;
-    }
-
+    /**
+     * This method sets the latitude value.
+     * @param value - latitude
+     */
+    public void setLatitude(double value){ this.lat = value;}
+    /**
+     * This method sets the id for the point.
+     * @param value - id
+     */
+    public void setId(int value){ this.id = value;}
+    /**
+     * This method returns a LatLng point with the latitude, longitude values.
+     * @return LatLng - new point to build polyline for tour.
+     */
     public LatLng getPoint(){
         LatLng point = new LatLng(this.lat,this.lng);
         return point;
     }
-
+    /**
+     * Basic toString method for TourPoint object.
+     * @return String - String representation of TourPoint object.
+     */
     public String toString(){
         String s = "("+ this.lat +", "+ this.lng+")";
         return s;
